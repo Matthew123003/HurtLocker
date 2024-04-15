5be59d0686ccc90189b42dbaa65e7c9e39d98633
Matt Kramer <matthew123003@gmail.com>
2024-04-15 10:25:33 -0400

    ListItems class done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -7,6 +11,14 @@ public class Main {
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
+    }
+
+    public String readData() throws FileNotFoundException {
+        File file = new File("/Users/matthew/Projects/HurtLocker/src/main/resources/RawData.txt");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNext()){
+
+        }
     }
 
     public static void main(String[] args) throws Exception{
