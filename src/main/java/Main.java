d9d73dec2910f841498d76fef3973d0dd338f8b8
Matt Kramer <matthew123003@gmail.com>
2024-04-15 17:44:37 -0400

    Thank You Google

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,10 @@
 import org.apache.commons.io.IOUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Main {
 
     public static String readRawDataToString() throws Exception{
         ClassLoader classLoader = Main.class.getClassLoader();
-        String result = IOUtils.toString(Objects.requireNonNull(classLoader.getResourceAsStream("RawData.txt")));
+        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
